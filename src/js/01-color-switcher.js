4c1a617e3df81c23808b2ef13ccd496bdb2fb238
SemSlipchenko <semslipchenko@gmail.com>
2022-09-10 15:02:52 +0300

    import flatpicker

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,28 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
 
 const bodyColor = document.querySelector('body')
 const startBtn = document.querySelector('[data-start]')
 const stopBtn = document.querySelector('[data-stop]')
-let startId = null;
-let startFlag = false;
+let startId = null
+let startFlag = false
 
 startBtn.addEventListener('click', onStartClick)
 stopBtn.addEventListener('click', onStopClick)
 
 function onStartClick() {
   if (startFlag) {
-    return;
+    return
   } else {
     startFlag = true;
     startId = setInterval(() => { 
     bodyColor.style.backgroundColor = getRandomHexColor()
   }, 1000) }
-  
 }
 
 function onStopClick() { 
   clearInterval(startId)
-  startFlag = false;
-  bodyColor.style.backgroundColor = '#FFFFFF';
+  startFlag = false
+  bodyColor.style.backgroundColor = '#FFFFFF'
 }
